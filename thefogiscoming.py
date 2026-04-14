ba950f290a0ea5007200da43fd26a615d33a45b7
Arturo Amberg <Arturo.amberg@ug.uchile.cl>
2026-04-14 00:28:18 -0400

    cambios

--- thefogiscoming.py
+++ thefogiscoming.py
@@ -1,8 +1,9 @@
 import discord
 import os
+import re
 
 TOKEN = os.environ.get("DISCORD_TOKEN")
-CHANNEL_NAME = "gassy"
+CHANNEL_NAMES = {"gassy", "meme-review"}
 FOG_EMOJI = "🌫️"
 
 intents = discord.Intents.default()
@@ -21,7 +22,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.name == CHANNEL_NAME:
+    if message.channel.name in CHANNEL_NAMES:
         content_lower = message.content.lower()
         if "the fog is coming" in content_lower:
             await message.channel.send("the fog is coming", tts=True)
@@ -31,7 +32,7 @@ async def on_message(message):
             await message.reply(file=discord.File("reari-dog-barking.gif"))
         elif "moreno" in content_lower:
             await message.reply(file=discord.File("daleMoreno.mp3"))
-        elif any(word in content_lower for word in ["war", "warhammer", "hammer"]):
+        elif re.search(r'\b(war|warhammer|hammer)\b', content_lower):
             await message.reply(file=discord.File("starking.webp"))
         elif "11" in message.content:
             await message.reply("chupalo entonces")
